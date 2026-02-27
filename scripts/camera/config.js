292cbf34f1fdc05d6ce6c40623d5160a658e8721
kanderson <81449406+kandrsn99@users.noreply.github.com>
2026-02-27 00:13:16 -0600

    Update config.js
    Fixed code alignment.

--- scripts/camera/config.js
+++ scripts/camera/config.js
@@ -1,9 +1,9 @@
 // Scene Configurations
 export const CAMERA_SETTINGS = {
-        FOV: 60, // Field of View
-        NEAR: 0.1, // Objects closer than this won't be drawn
-        FAR: 20000, // Objects further than this won't be drawn
-        START_Z: 1500 // Starting view distance
+    FOV: 60, // Field of View
+    NEAR: 0.1, // Objects closer than this won't be drawn
+    FAR: 20000, // Objects further than this won't be drawn
+    START_Z: 1500 // Starting view distance
 };
 // Region view Settings
 export const REGION_SETTINGS = {
@@ -17,17 +17,17 @@ export const REGION_SETTINGS = {
 };
 // Colors for different types of stars from O, B, A, F, G, K, to M. O being hot and M being cool.
 export const STAR_COLORS = {
-        'O': 0x9bb0ff,
-        'B': 0xaabfff,
-        'A': 0xcad7ff,
-        'F': 0xf8f7ff,
-        'G': 0xfff4ea,
-        'K': 0xffd2a1,
-        'M': 0xffad51,
-        'L': 0x8b4513,
-        'T': 0x654321,
-        'Y': 0x400080,
-        'DEFAULT': 0xffffff
+	'O': 0x9bb0ff,
+	'B': 0xaabfff,
+    'A': 0xcad7ff,
+    'F': 0xf8f7ff,
+    'G': 0xfff4ea,
+    'K': 0xffd2a1,
+    'M': 0xffad51,
+    'L': 0x8b4513,
+    'T': 0x654321,
+    'Y': 0x400080,
+    'DEFAULT': 0xffffff
 };
 // Color of heatmapping
 export const HEATMAP_COLORS = {
